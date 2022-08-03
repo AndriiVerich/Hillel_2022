82e588b1e78d5ffb9204c2ac99f8e073ebf52b26
AndriiVerich <av21153@gmail.com>
2022-08-03 23:23:12 +0300

    First homework solution

---
+++ lesson_3_1.py
@@ -0,0 +1,36 @@
+# Создать 3 переменные с одинаковыми данными и с одинаковыми идентификаторами
+var_1 = "12345"
+var_2 = var_1
+var_3 = var_2
+print("Первая часть")
+print("Переменные: ", var_1, var_2, var_3)
+print('ID :',id(var_1), id(var_2), id(var_3),'\n')
+# Поменять их типы так, чтобы у 1-х трёх стали разные идентификаторы, но при этом остались одинаковые данные
+var_1 = tuple(var_1)
+var_2 = float(var_2)
+var_3 = int(var_3)
+
+print("Переменные: ", var_1, var_2, var_3)
+print('ID :',id(var_1), id(var_2), id(var_3),'\n')
+
+#Создать 2 переменные с одинаковыми данными и с разными идентификаторами
+
+a1 = 1
+a2 = 1
+print("Вторая часть")
+#print('ID:', id(a1))
+#print('ID:',id(a2))
+print('a1 == a2')
+print(a1 == a2)
+print('a1 is a2')
+print(a1 is a2)
+a1 = bool(a1)
+a2 = float(a2)
+#print('ID:', id(a1))
+#print('ID:',id(a2))
+print('a1 == a2')
+print(a1 == a2)
+print('a1 is a2')
+print(a1 is a2)
+
+
