80b53a3dcdbc4e14d773bf027eae694d338de3c6
AndriiVerich <av21153@gmail.com>
2022-08-06 12:56:42 +0300

    Home work for lesson 4

---
+++ lesson_04_hw.py
@@ -0,0 +1,14 @@
+
+input_word_1 = input("Введите два слова: ").split()
+
+word_new_1 = "! " + str(input_word_1[1]).upper() + " " + str(input_word_1[0]).title() + "?"
+word_new_2 = "! {} {}?".format(input_word_1[1].upper(), input_word_1[0].title())
+word_new_3 = f"! {input_word_1[1].upper()} {input_word_1[0].title()}?"
+
+s_file= open("python.txt", "w")
+
+print(word_new_1, file=s_file)
+print(word_new_2, file=s_file)
+print(word_new_3, file=s_file)
+
+s_file.close()
