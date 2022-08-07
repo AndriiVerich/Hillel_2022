fa4f662c9106c7f9a3309eaaba606f22b71d505f
AndriiVerich <av21153@gmail.com>
2022-08-07 14:24:55 +0300

    Retake home work for lesson 3

--- lesson_3_1.py
+++ lesson_3_1.py
@@ -1,13 +1,13 @@
 # Создать 3 переменные с одинаковыми данными и с одинаковыми идентификаторами
 var_1 = "12345"
-var_2 = var_1
-var_3 = var_2
+var_2 = "12345"
+var_3 = "12345"
 print("Первая часть")
 print("Переменные: ", var_1, var_2, var_3)
 print('ID :',id(var_1), id(var_2), id(var_3),'\n')
 # Поменять их типы так, чтобы у 1-х трёх стали разные идентификаторы, но при этом остались одинаковые данные
-var_1 = ''.join(list(var_1))
-var_2 = float(var_2)
+var_1 = int(var_1)
+var_2 = int(var_2)
 var_3 = int(var_3)
 
 print("Переменные: ", var_1, var_2, var_3)
@@ -16,18 +16,18 @@ print('ID :',id(var_1), id(var_2), id(var_3),'\n')
 #Создать 2 переменные с одинаковыми данными и с разными идентификаторами
 
 a1 = 1
-a2 = 1
+a2 = 1.0
 print("Вторая часть")
-#print('ID:', id(a1))
-#print('ID:',id(a2))
+print('ID:', id(a1))
+print('ID:',id(a2))
 print('a1 == a2')
 print(a1 == a2)
 print('a1 is a2')
 print(a1 is a2)
 a1 = bool(a1)
-a2 = float(a2)
-#print('ID:', id(a1))
-#print('ID:',id(a2))
+a2 = bool(a2)
+print('ID:', id(a1))
+print('ID:',id(a2))
 print('a1 == a2')
 print(a1 == a2)
 print('a1 is a2')
