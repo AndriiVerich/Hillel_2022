2fac985d17e7163d1e5e95c4f732f7485be24969
AndriiVerich <av21153@gmail.com>
2022-08-17 23:27:00 +0300

    Home work for lesson 7

--- lesson_7/lesson_7_lambda.py
+++ lesson_7/lesson_7_lambda.py
@@ -2,5 +2,5 @@ int_parity = 2
 int_noparity = 3
 parity = lambda a: "Четное" if a % 2 == 0 else "Не четное"
 
-print(parity(int_parity))
-print(parity(int_noparity))
+print(int_parity, parity(int_parity))
+print(int_noparity, parity(int_noparity))
