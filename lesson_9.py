63da81944eab42b929685f6c4d4c5d4db64270ee
AndriiVerich <av21153@gmail.com>
2022-08-25 16:18:11 +0300

    Apdate Home work for lesson 9

--- lesson_9.py
+++ lesson_9.py
@@ -1,20 +1,30 @@
 def recognize(word: str):
+    word = word.replace(",", ".", 1)
     try:
-        if "." in word:
+        word.replace(",", ".", 1)
+        if float(word) == 0:
+            return f"Вы ввели число {int(float(word))}"
+        elif "." in word :
             return "Вы ввели {} дробное число {}".format("положительное" if float(word) > 0 else "отрицательное", float(word))
         else:
             return "Вы ввели {} целое число {}".format("положительное" if int(word) > 0 else "отрицательное", int(word))
+
     except ValueError:
         return f"Вы ввели не корректное число: {word}"
 
 
 def recognize_2(word: str):
+    word = word.replace(",", ".", 1)
     if not word.replace(".", "", 1).replace("-", "", 1).isdigit():
         return f"Вы ввели не корректное число: {word}"
-    elif "." in word:
+    elif float(word) == 0:
+        return f"Вы ввели число {int(float(word))}"
+    elif "." in word :
         return "Вы ввели {} дробное число {}".format("положительное" if float(word) > 0 else "отрицательное", float(word))
-    else:
+    else :
         return "Вы ввели {} целое число {}".format("положительное" if int(word) > 0 else "отрицательное", int(word))
+
+
 
 
 while True:
