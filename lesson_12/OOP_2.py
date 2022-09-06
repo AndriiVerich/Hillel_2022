08bf596b0d1137594e1c39af2812c495be941292
AndriiVerich <av21153@gmail.com>
2022-09-06 10:05:50 +0300

    Complete OOP2

--- lesson_12/OOP_2.py
+++ lesson_12/OOP_2.py
@@ -4,14 +4,37 @@
 # его реализацию сделать при помощи оператора super.
 # А так же дополнительный метод load. При его вызове происходит пауза 1 сек.,
 # затем выдаётся сообщение «load» и снова пауза 1 сек.
+
 # Класс car имеет дополнительный обязательный атрибут max_speed и при вызове
 # метода move, после появления надписи «move» должна появиться надпись
 # «max speed is <max_speed>». Вместо <max_speed> должно выводится значение обязательного атрибума max_speed.
 # Создать по 2 объекта для каждого из классов truck и car, проверить все их методы и атрибуты.
-import OOP_1
+import time
 
 
-class Truck(OOP_1.Auto):
+class Auto(object):
+    brand = str
+    age = int
+    color = str
+    mark = str
+    weight = int
+
+    def __init__(self, brand, age, mark):
+        self.age = age
+        self.brand = brand
+        self.mark = mark
+
+    def move(self):
+        print('«move»')
+
+    def stop(self):
+        print('«stop»')
+
+    def birthday(self):
+        self.age += 1
+
+
+class Truck(Auto):
     max_load = int
 
     def __init__(self, brand, age, mark, max_load):
@@ -22,6 +45,36 @@ class Truck(OOP_1.Auto):
         print('«attention»')
         super().move()
 
+    def load(self):
+        time.sleep(1)
+        print('«load»')
+        time.sleep(1)
+
+
+class Car(Auto):
+    max_speed = int
+
+    def __init__(self, brand, age, mark, max_speed):
+        super().__init__(brand, age, mark)
+        self.max_speed = max_speed
+
+    def move(self):
+        super().move()
+        print(f'max speed is {self.max_speed}')
+
 
 truck_1 = Truck('BMV', 10, 'E100', 5000)
+truck_2 = Truck('BMV', 5, 'E23', 10000)
+car_1 = Car('BMV', 10, 'E320', 150)
+car_2 = Car('Mazda', 7, '450', 180)
+
 truck_1.move()
+truck_1.load()
+print('-'* 50)
+
+car_1.move()
+car_2.move()
+car_1.stop()
+print(car_2.age)
+car_2.birthday()
+print(car_2.age)
